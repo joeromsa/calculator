4f64ce3e9145797687b47c19077db4fa4fdab273
joeromsa <jromsa6@gmail.com>
2022-05-17 21:11:14 -0500

    Header display done

--- calculator/src/components/CalcHeader.js
+++ calculator/src/components/CalcHeader.js
@@ -1,9 +1,14 @@
 import React from "react";
+import light from '../img/light.svg';
 import '../styles/Header.css';
 
 const CalcHeader = () => {
     return (
         <div className="header">
+            <div className="title">
+                <div className="text">Calculator</div>    
+            </div>
+            <div className="displayMode" style={{ backgroundImage: `url(${light})` }}></div>
         </div>
     );
 }
